f737f6b06796dd66f30536be1362f9433795acbf
duankun <duankun1993@126.com>
2025-02-26 18:49:20 +0800

    cli update

--- nestjs/nest-cli/src/app.module.ts
+++ nestjs/nest-cli/src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { OrderModule } from './order/order.module';
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, OrderModule],
   controllers: [],
   providers: [],
 })
